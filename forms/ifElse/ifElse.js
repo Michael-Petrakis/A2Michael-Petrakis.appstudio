de1f5104ebcbb43268f64248317596b06ba47cac
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-09-19 13:41:06 -0500

    Version 4

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,4 +1,20 @@
+let name = prompt("Enter your first name")
+
 let state = prompt("Enter the state you live in. (Two capitalized letters)")
 
-let temp = prompt("Enter the current outside temperature")
+let temp = prompt("Enter the current outside temperature (In fahrenheit)")
 
+let messages = ['wear a warm coat, hat, scarf, and gloves', 'wear a warm coat but you won\'t need a hat, scarf, or gloves', 'wear your warmest coat, a warm hat, a scarf, and warm gloves', 'wear a warm coat, hat and gloves. Maybe a scarf too']
+
+if (temp < 32 && state == 'NE') {
+  console.log(`${name}, you should ${messages[0]}`)
+}
+else if (temp >= 32 && temp < 50 && state == 'NE') {
+  console.log(`${name}, you should ${messages[1]}`)
+}
+else if (temp >= 32 && temp > 50 && state == 'FL') {
+  console.log(`${name}, you should ${messages[2]}`)
+}
+else if (temp >= 50 && temp < 70 && state == 'FL') {
+  console.log(`${name}, you should ${messages[3]}`)
+}
--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,3 +1,9 @@
+/*
+let name = prompt("Enter your first name")
+
 let state = prompt("Enter the state you live in. (Two capitalized letters)")
 
-let temp = prompt("Enter the current outside temperature")
+let temp = prompt("Enter the current outside temperature (In fahrenheit)")
+
+let messages = ['Wear a warm coat, hat, scarf, and gloves', "wear a warm coat but you won'tneed a hat, scarf, or gloves", "wear your warmest coat, a warm hat, a scarf, and warm gloves", "wear a warm coat, hat and gloves. Maybe a scarf too")
+*/
