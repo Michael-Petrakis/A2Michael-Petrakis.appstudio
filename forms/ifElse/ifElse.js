af2eb8e10bcf96a4ce2c9acce327351842be0566
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-09-19 14:41:10 -0500

    Version 4

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,20 +1,41 @@
+
+//Retrieves the name of user and stores in variable
 let name = prompt("Enter your first name")
 
+//Retrieves the state of user and stores in variable
 let state = prompt("Enter the state you live in. (Two capitalized letters)")
 
+//Retrieves the temperature of user and stores in variable
 let temp = prompt("Enter the current outside temperature (In fahrenheit)")
 
+//Array that stores the messages displayed depending on what state and temperature is entered
 let messages = ['wear a warm coat, hat, scarf, and gloves', 'wear a warm coat but you won\'t need a hat, scarf, or gloves', 'wear your warmest coat, a warm hat, a scarf, and warm gloves', 'wear a warm coat, hat and gloves. Maybe a scarf too']
 
+//If statement that displays what should be worn based on temperature and state
 if (temp < 32 && state == 'NE') {
   console.log(`${name}, you should ${messages[0]}`)
 }
+
+//If statement that displays what should be worn based on temperature and state
 else if (temp >= 32 && temp < 50 && state == 'NE') {
   console.log(`${name}, you should ${messages[1]}`)
 }
-else if (temp >= 32 && temp > 50 && state == 'FL') {
+
+//If statement that displays what should be worn based on temperature and state
+else if (temp >= 32 && temp < 50 && state == 'FL') {
   console.log(`${name}, you should ${messages[2]}`)
 }
+
+//If statement that displays what should be worn based on temperature and state
 else if (temp >= 50 && temp < 70 && state == 'FL') {
   console.log(`${name}, you should ${messages[3]}`)
 }
+
+//If statement that lets the user know they did not enter a valid temperature
+else if (temp >= 70) {
+  console.log(`${name}, you didn't enter valid inputs`)
+}
+
+//If statement that lets the user know they did not enter a valid state
+else (state != 'NE' || 'FL') 
+  console.log(`${name}, you didn't enter valid inputs`)
--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -1,9 +1,25 @@
 /*
+//Retrieves the name of user and stores in variable
 let name = prompt("Enter your first name")
 
+//Retrieves the state of user and stores in variable
 let state = prompt("Enter the state you live in. (Two capitalized letters)")
 
+//Retrieves the temperature of user and stores in variable
 let temp = prompt("Enter the current outside temperature (In fahrenheit)")
 
-let messages = ['Wear a warm coat, hat, scarf, and gloves', "wear a warm coat but you won'tneed a hat, scarf, or gloves", "wear your warmest coat, a warm hat, a scarf, and warm gloves", "wear a warm coat, hat and gloves. Maybe a scarf too")
+//Array that stores the messages displayed depending on what state and temperature is entered
+let messages = ['wear a warm coat, hat, scarf, and gloves', 'wear a warm coat but you won\'t need a hat, scarf, or gloves', 'wear your warmest coat, a warm hat, a scarf, and warm gloves', 'wear a warm coat, hat and gloves. Maybe a scarf too']
+
+switch () {
+case temp < 32 && state == 'NE':
+  console.log(`${name}, you should ${messages[0]}`)
+    break;
+case temp >= 32 && temp < 50 && state == 'NE':
+    console.log(`${name}, you should ${messages[1]}`)
+    break;
+default: 
+  console.log(`${name}, you didn't enter valid inputs`)
+  break;
+}
 */
